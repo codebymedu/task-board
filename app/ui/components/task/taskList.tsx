d12a5e153a123535d72360ac58796017d904c82d
Medu <03medu@gmail.com>
2024-05-13 10:31:52 +0200

    feat: empty state

--- app/ui/components/task/taskList.tsx
+++ app/ui/components/task/taskList.tsx
@@ -9,6 +9,18 @@ export const TaskList = async ({ taskboardId }: TaskListProps) => {
 
   // --- RENDER ---
 
+  if (!tasks?.length) {
+    return (
+      <div className="w-full text-center my-4 mt-8 p-4 rounded-lg border-2 border-dashed border-gray-300">
+        {" "}
+        <h3 className=" text-sm font-semibold text-gray-900">No tasks yet</h3>
+        <p className="mt-1 text-sm text-gray-500">
+          Get started by creating a new task.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <section>
       <Suspense>
